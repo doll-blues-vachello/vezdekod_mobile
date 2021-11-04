50aef19e81eae03b5f4b1027ebc445d5ebd2b4ae
Vladislav Kheynov <lcastory86@gmail.com>
2021-11-04 18:00:39 +0700

    feat: basic player functionality

--- app/src/main/java/ru/kheynov/vezdekodapp/MainActivity.kt
+++ app/src/main/java/ru/kheynov/vezdekodapp/MainActivity.kt
@@ -1,11 +1,61 @@
 package ru.kheynov.vezdekodapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.ui.PlayerView
+import androidx.core.app.ActivityCompat
+
+import android.content.pm.PackageManager
+
+import android.app.Activity
+
+
+
 
 class MainActivity : AppCompatActivity() {
+	private val REQUEST_EXTERNAL_STORAGE = 1
+	private val PERMISSIONS_STORAGE = arrayOf<String>(
+		Manifest.permission.READ_EXTERNAL_STORAGE,
+		Manifest.permission.WRITE_EXTERNAL_STORAGE
+	)
+	val videoUri = "file:///storage/emulated/0/videos/rickroll.mp4"
+	private lateinit var playerInstance: SimpleExoPlayer
+
+	fun verifyStoragePermissions(activity: Activity?) {
+		// Check if we have write permission
+		val permission = ActivityCompat.checkSelfPermission(activity!!,
+			Manifest.permission.WRITE_EXTERNAL_STORAGE)
+		if (permission != PackageManager.PERMISSION_GRANTED) {
+			// We don't have permission so prompt the user
+			ActivityCompat.requestPermissions(
+				activity,
+				PERMISSIONS_STORAGE,
+				REQUEST_EXTERNAL_STORAGE
+			)
+		}
+	}
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+
+		verifyStoragePermissions(this)
+		val playerView: PlayerView = findViewById(R.id.player_view)
+
+		playerInstance = SimpleExoPlayer.Builder(this).build()
+		playerView.player = playerInstance
+		val mediaItem = MediaItem.fromUri(Uri.parse(videoUri))
+		playerInstance.setMediaItem(mediaItem)
+		playerInstance.prepare()
+		playerInstance.play()
+	}
+
+	override fun onDestroy() {
+		playerInstance.release()
+		super.onDestroy()
+
 	}
 }
